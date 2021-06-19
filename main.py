42b1b6efb2e509a9d1830d41a80d2c139e5dc3a8
Bengemon825 <62958342+Bengemon825@users.noreply.github.com>
2021-06-19 18:03:47 -0400

    work on p&l

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cbpro
 import matplotlib.pyplot as plt
 import pandas as pd
 import graphics  # personal module
-from PIL import Image
 
 public_client = cbpro.PublicClient()
 
@@ -26,5 +25,29 @@ graphics.GenerateImage("BTC-USD", btc_price, btc_df)
 eth_price = public_client.get_product_ticker(product_id='ETH-USD').get("price")
 graphics.GenerateImage("ETH-USD", eth_price, eth_df)
 coins = [("BTC", btc_price), ("ETH", eth_price)]
-graphics.four_boxes_image(coins)
+# graphics.four_boxes_image(coins)
 # ----------------------------------------------------------------------------------------
+"""Calculate P&L using previous day's close"""
+eth_24hr = public_client.get_product_24hr_stats('ETH-USD')
+# print(eth_24hr)
+eth_last = eth_24hr.get('last')
+eth_current_high = eth_df['high'].loc[0]
+# TODO: high based on current granularity, might want to do a separate poll for this section in case user sets
+#       the granularity higher (edge case would be comparing 1-day to 1-day)
+
+# print("Current high:", eth_current_high, "Close:", eth_last)
+eth_last = float(eth_last)
+eth_pl = ((eth_last - eth_current_high) / eth_last) * 100
+# pl = '{0:.2f}'.format(pl)
+eth_pl = round(eth_pl, 2)
+print("ETH P&L:", eth_pl)
+
+btc_24hr = public_client.get_product_24hr_stats('BTC-USD')
+# print(btc_24hr)
+btc_last = btc_24hr.get('last')
+btc_current_high = btc_df['high'].loc[0]
+# print("Current high:", btc_current_high, "Close:", btc_last)
+btc_last = float(btc_last)
+btc_pl = ((btc_last - btc_current_high) / btc_last) * 100
+btc_pl = round(btc_pl, 2)
+print("BTC P&L:", btc_pl)
